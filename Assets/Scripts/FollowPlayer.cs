961fc6ade9022f283767cb676e82cd76558db1f2
Marc Zaku <mzaku@hotmail.de>
2023-10-09 11:29:57 +0200

    Fix Exception On Player Death

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,10 @@ public class FollowPlayer : MonoBehaviour
     private void LateUpdate()
     {
         GameObject player = GameObject.Find("Player");
+        if (player == null)
+        {
+            return;
+        }
         Vector3 target = player.transform.position + new Vector3(0, 1.38f, -4.83f);
         transform.position = target;
     }
